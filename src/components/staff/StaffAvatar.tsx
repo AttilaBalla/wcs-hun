044ae567811e10054e690005f0cfcd798881c089
AttilaBalla <xattus@gmail.com>
2024-07-16 00:36:24 +0200

    add img prefix to fix img paths on GH pages

--- src/components/staff/StaffAvatar.tsx
+++ src/components/staff/StaffAvatar.tsx
@@ -4,6 +4,7 @@ import LocalPoliceIcon from '@mui/icons-material/LocalPolice';
 import {Avatar, useTheme} from "@mui/material";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import {imgPrefix} from "@/utils/imgPrefix";
 
 interface IProps {
   name: string
@@ -17,7 +18,7 @@ export function StaffAvatar(props: IProps) {
 
   return (
     <Box display={'flex'} sx={{alignItems: 'center', margin: '0 2rem'}}>
-      <Avatar src={`/static/images/${imgPath}`} sx={{backgroundColor: theme.palette.secondary.main, mr: '1rem'}}>
+      <Avatar src={`${imgPrefix}/static/images/${imgPath}`} sx={{backgroundColor: theme.palette.secondary.main, mr: '1rem'}}>
         {imgPath ? null : <LocalPoliceIcon/>}
       </Avatar>
       <Typography variant='subtitle2' component={'h3'}>{name}</Typography>
