cb5749c188a9c7bae739965d67c30004f979dfff
blaselinux <blaselinux@gmail.com>
2024-08-08 18:29:26 +0200

    Registration text update

--- src/views/RegistrationView.tsx
+++ src/views/RegistrationView.tsx
@@ -23,6 +23,8 @@ export function RegistrationView() {
       </Typography>
       <Stack spacing={3}>
         <Typography>
+          Elővételes regisztráció 2024.08.15-ig, a hozzájárulást pedig 2024.08.16-ig kell befizetni regisztráció után. 
+          Elővételes regisztráció esetén az alábbi kedvezményt tudjuk biztosítani a táncosok résztvevői hozzájárulásából. 
           Ha még nem versenyeztél, de szeretnéd kipróbálni és megtudni milyen érzés, miért ne tennéd ezt a barátaid,
           ismerőseid körében?
         </Typography>
@@ -35,12 +37,12 @@ export function RegistrationView() {
             * Advanced és AllStar szintű versenyzőknek a nevezés ingyenes.
           </Typography>
         </Box>
-        <Box display={'flex'} sx={{alignItems: 'center'}}>
+/*        <Box display={'flex'} sx={{alignItems: 'center'}}>
           <InfoIcon color={'secondary'} sx={{mr: '.5rem', mb: '3px'}}/>
           <Typography>
             Elővételes regisztráció: 2024.08.15-ig, befizetés: 2024.08.16-ig
           </Typography>
-        </Box>
+        </Box> */
         <Box>
           <Typography>
             Az eseményen táncosként, versenyzőként és nézőként való részvétellel, minden személy:
